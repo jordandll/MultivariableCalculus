daf2b8d64404e1f819805c043e18be7133b391ca
Jordan Driscoll <drisjor@protonmail.com>
2024-05-07 12:38:30 -0400

    Update the ancillary draw_flow.py script.

--- DoubleIntegrals/GreensTheorem/pset/draw_flow.py
+++ DoubleIntegrals/GreensTheorem/pset/draw_flow.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.animation as animation
 
+from itertools import product
+
 
 # Create the domain of the density field, denoted as rho.
 X = np.linspace(0.25,3,num=50)
@@ -25,11 +27,11 @@ F = (rho_F*v[0],rho_F*v[1])
 
 """ Define the position vector 'r', which tracks the movement of 'P'"""
 x_0, y_0 = 1, 3
-def r_x(t):
+def r_x(t, x_0):
     return x_0 + t
 
-def r_y(t):
-    return x_0*y_0/r_x(t)
+def r_y(t, x_0, y_0):
+    return x_0*y_0/r_x(t, x_0)
 
 # Create the figure and axes.
 fig, ax = plt.subplots()
@@ -50,11 +52,15 @@ ax.quiver(X_v,Y_v,*v,angles='uv',scale=4,scale_units='inches',
 ax.barbs(X_v,Y_v,*F)
 
 # Animate the motion of 'P'.
-P, = ax.plot([],[],marker='o',color='tab:red')
+x = np.linspace(0.5,2.5,num=5)
+y = np.linspace(1,3, num = 3)
+xy = list(product(x,y))
+P = ax.plot(*[[] for i in range(2*len(xy))],marker='o',color='tab:blue')
 
 def update(frame):
-    P.set_data([r_x(frame)], [r_y(frame)])
-    return P,
+    for p, p_i in zip(P,xy):
+        p.set_data([r_x(frame,p_i[0])],[r_y(frame,*p_i)])
+    return P
 
 ani = animation.FuncAnimation(fig, update, frames = t, interval = 50)
 
